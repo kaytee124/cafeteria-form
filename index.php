2dc5d611a4e9ac6ecb9921ce43b58e72ab5ff852
kaytee124 <ktproductions124@gmail.com>
2025-04-13 02:19:03 +0000

    vercelremoved

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-header('Content-Type: text/html; charset=UTF-8');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once("controller/meal_controller.php");
 ?>
 <!DOCTYPE html>
